e779a51ef6a8e4c3977ba481a69fac8e4abb6596
sobaya <sobaya007@gmail.com>
2019-12-17 11:25:58 +0900

    Add: Multi line support

--- source/app.d
+++ source/app.d
@@ -3,10 +3,17 @@ import repld;
 
 void main() {
     auto runner = new REPLRunner();
+    string input;
+
     while (true) {
-        write("> ");
-        auto input = readln;
-        auto result = runner.run(input);
+        write(input ? "| " : "> ");
+        input ~= readln;
+
+        if (input.stripRight.endsWith("|")) continue;
+
+        auto result = runner.run(input.split("\n").map!(line => line.stripRight(" |")).join("\n"));
+
+        scope (exit) input = null;
         if (!result.success)
             writeln(result.message);
     }
